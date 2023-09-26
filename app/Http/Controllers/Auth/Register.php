7ca8e0bb36b06fa7b4c1e312a6565fde1b464370
MMahdiSheykh <momoaban@gmail.com>
2023-09-26 17:05:21 +0330

    cleaning Register.php class & redirect user to event.create route

--- app/Http/Controllers/Auth/Register.php
+++ app/Http/Controllers/Auth/Register.php
@@ -10,25 +10,11 @@ use Illuminate\Validation\Rule;
 
 class Register extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return view('auth.register');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -52,37 +38,6 @@ class Register extends Controller
             'role' => UserRoleEnum::SIMPLE_USER,
         ]);
 
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return redirect(route('event.create'));
     }
 }
